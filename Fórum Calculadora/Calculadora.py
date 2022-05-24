f94480e5f2c52238c5b1c0978df83092f7c53eaf
Pedro Henrique Arruzzo <pharruzzo@gmail.com>
2022-05-23 21:34:14 -0300

    Update Calculadora.py

--- Fórum Calculadora/Calculadora.py
+++ Fórum Calculadora/Calculadora.py
@@ -1,5 +1,6 @@
 from xmlrpc.client import boolean
 import operator
+import math
 
 
 class Calculadora:
@@ -28,38 +29,67 @@ class Calculadora:
 
     def calculo(self):
         print("Para finalizar o programa, aperte 'enter' sem digitar uma operação")
+        
+        # Requisita o valor de X.
         x = input("Digite um valor: ")
+        
+        # Caso o valor retorne nulo, irá requisitar o valor novamente.
         if x == "":
             while x == "":
                 x = input("Digite um valor: ")
         x = float(x)
 
+        # Esta variável irá guardar o valor de 'operacao'.
         op = 0
 
-        while x != "":
-            operacao = input("Digite a operação (+, -, *, /, %, **, //): ")
+        # Aqui inicializo y para evitar erros.
+        y = 0
 
-            if operacao == "=":
+        # Aqui começa os cálculos.
+        # Está dentro de um while pois você pode realizar operações com o resultado da anterior.
+        while x != "":
+            operacao = input("Digite a operação (+, -, *, /, %, **, //, =, sqr): ")
+            
+            # Caso o usuário inserir '=' como operação, o programa irá executar a operação anterior com o resultado mais recente.
+            if operacao == "=" and op == 0:
+                print("Operação inválida")
+                continue
+
+            if operacao == "=" and op != 0:
                 self.i = self.operador[op](x,y)
                 print(calculadora.i)
                 x = self.i
                 continue
 
+            # Realiza a raíz quadrada utilizando o módulo math que foi importado.
+            if operacao == "sqr":
+                self.i = math.sqrt(x)
+                print(calculadora.i)
+                x = self.i
+                continue
+
+            # Caso não insira nenhum valor em operacao, o programa será finalizado.
             if operacao == "":
                 print("Operação finalizada")
                 break
 
-            op = operacao
+            # Requisita o valor de Y.
             y = input("Digite um valor: ")
+            if y == "":
+                y = 0
             y= float(y)
+            op = operacao
             
+            # Realiza as operações aritiméticas utilizando o módulo operator que foi importado.
             if operacao == "+" or operacao == "-" or operacao == "*" or operacao == "/" or operacao == "%" or operacao == "**" or operacao == "//":
                 self.i = self.operador[operacao](x,y)
             
+            # Caso operacao não possua um valor válido, a aplicação pedirá para digitar novamente.
             else:
                 print("Operação inválida")
-                break
+                continue
 
+            # Mostra o resultado da operação no output
             print(calculadora.i)
             x = self.i
         
@@ -69,7 +99,7 @@ calculadora = Calculadora()
 
 calculadora.calculo()
 
-# Eu empolguei. (Queria fazer mais só que fiquei com fome)
+# Eu empolguei. (Queria fazer mais só que fiquei com fome e acabei parando)
 
 
 
